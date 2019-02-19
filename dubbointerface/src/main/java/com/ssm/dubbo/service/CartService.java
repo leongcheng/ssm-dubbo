3f4b28ff9de7634415dd562f6d2130193fea5e7e
leongcheng <leongbecome@163.com>
2019-02-19 21:42:40 +0800

    代码整合

---
+++ dubbointerface/src/main/java/com/ssm/dubbo/service/CartService.java
@@ -0,0 +1,12 @@
+package com.ssm.dubbo.service;
+
+/**
+ * 购物车
+ * Created by Administrator on 2019/2/11 0011 上午 1:22
+ */
+public interface CartService {
+
+
+
+
+}
---
+++ dubbointerface/src/main/java/com/ssm/dubbo/service/SsoService.java
@@ -0,0 +1,16 @@
+package com.ssm.dubbo.service;
+
+/**
+ * 单点登录
+ * Created by Administrator on 2019/2/11 0011 上午 10:35
+ */
+public interface SsoService {
+
+
+
+
+
+
+
+
+}
